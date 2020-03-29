8aa7fd7ac02a93c7b83d82943ea157a160d1792a
ntorresp <n.torresp@uniandes.edu.co>
2020-03-29 11:17:12 -0500

    Paso 2 - Se creo el test para crear portafolios y listar los existentes (VERDE)

--- portafolio/views.py
+++ portafolio/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
+from .models import Portafolio
 
 # Create your views here.
 @csrf_exempt
 def index(request):
-    images_list = []
+    images_list = Portafolio.objects.all()
     return HttpResponse(serializers.serialize("json", images_list))
