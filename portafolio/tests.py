859c031a2f254fe5e6094b027e892ed6f624500f
Sebastian <js.millan10@uniandes.edu.co>
2020-03-29 10:17:12 -0500

    Paso 1 - Se creo el test para listar los portafolios existentes (ROJO)

--- portafolio/tests.py
+++ portafolio/tests.py
@@ -5,8 +5,8 @@ from django.test import TestCase, Client
 from .models import Image
 import json
 
-
-def test_list_portafolio_status(self):
-    url = '/gallery/'
-    response = self.client.get(url, format='json')
-    self.assertEqual(response.status_code, 200)
+class PortafolioTestCase(TestCase):
+    def test_list_portafolio_status(self):
+        url = '/portafolio/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, 200)
