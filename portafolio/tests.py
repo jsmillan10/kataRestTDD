0abb53424cc3c36fd55523690da83178700aaa47
Sebastian <js.millan10@uniandes.edu.co>
2020-03-29 11:21:43 -0500

    Paso 3 - Se creo el test para crear un usuario y verificar su información (ROJO)

--- portafolio/tests.py
+++ portafolio/tests.py
@@ -11,7 +11,7 @@ class PortafolioTestCase(TestCase):
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, 200)
 
-    def test_count_images_list(self):
+    def test_count_portafolio_list(self):
         user_model = User.objects.create_user(username='test', password='kd8wke-DE34', first_name='test',
                                               last_name='test', email='test@test.com')
         Portafolio.objects.create(name='nuevo', url='No', description='testImage', type='jpg', user=user_model)
@@ -22,3 +22,8 @@ class PortafolioTestCase(TestCase):
         response = self.client.get('/portafolio/')
         current_data = json.loads(response.content)
         self.assertEqual(len(current_data), 4)
+
+    def test_add_user(self):
+        response=self.client.post('/gallery/addUser/',json.dumps({"username": "testUser", "first_name": "Test", "last_name": "User", "password": "AnyPas#5", "email": "test@test.com"}), content_type='application/json')
+        current_data=json.loads(response.content)
+        self.assertEqual(current_data[0]['fields']['username'],'testUser')
