b935bdbb4d0ea309d09826505b4bde226dfc896b
Sebastian <js.millan10@uniandes.edu.co>
2020-03-29 09:40:27 -0500

    Se agregaron los imports

--- portafolio/tests.py
+++ portafolio/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
 
 # Create your tests here.
+from .models import Image
+import json
+
